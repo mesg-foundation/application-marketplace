da53951c1957981ac8a669a9aad7cf84b0e138b6
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-03-08 10:12:52 +0300

    cache services on marketplace change events
    * utils: add utils for MESG's task executions and event listenings.
    * funcs: rm funcs packages.

--- index.js
+++ index.js
@@ -2,36 +2,52 @@ const endpoint = process.env.coreAddr || 'localhost:50052'
 const mesg = require('mesg-js').application({ endpoint })
 const debug = require('debug')('marketplace')
 const error = require('debug')('marketplace:error')
-const { list } = require('./funcs/marketplace')
-const {
-  listenRequests,
-  response,
-  cacheResponse,
-} = require('./funcs/http-server')
+const executeTask = require('./utils/mesg.executeTask')(mesg)
+const listenEvent = require('./utils/mesg.listenEvent')(mesg)
+
+// update services cache on startup.
+cacheServicesResponse()
+
+// update services cache every 2 mins.
+setInterval(cacheServicesResponse, 60 * 1000 * 2)
+
+// update services cache by watching marketplace events.
+;[
+  'serviceCreated',
+  'serviceOfferCreated',
+  'serviceOfferDisabled',
+  'serviceOwnershipTransferred',
+  'servicePurchased',
+  'serviceVersionCreated'
+].forEach((eventKey) => {
+  listenEvent('marketplace', eventKey)
+    .on('data', cacheServicesResponse)
+    .on('error', (err) => {
+      error(`err while listening event ${eventKey}:`, err)
+      process.exit(1)
+    })
+})
 
 // cacheServicesResponse caches response for /services JSON API.
 async function cacheServicesResponse() {
   debug('caching response for services...')
-  await cacheResponse(mesg, {
+  await executeTask('http-server', 'cache', {
     method: 'get',
     path: '/services',
     code: 200,
     mimeType: 'application/json',
-    content: JSON.stringify(await list(mesg))
+    content: JSON.stringify(await executeTask('marketplace', 'listServices'))
   })
   debug('response cached for services')
 }
 
-cacheServicesResponse()
-setInterval(cacheServicesResponse, 60 * 1000 * 2)
-
 // serve 404 for * JSON API.
-listenRequests(mesg)
+listenEvent('http-server', 'request')
   .on('data', async (event) => {
     const data = JSON.parse(event.eventData)
     const sessionID = data.sessionID
     try {
-      await response(mesg, { sessionID, code: 404 })
+      await executeTask('http-server', 'completeSession', { sessionID, code: 404 })
     } catch(err) {
       error('error while responding api request:', err)
     }
