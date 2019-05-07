d905cbd94977a2e9ecbab0bd60d95fb84735f3c5
Anthony ESTEBE <anthony@mesg.com>
2019-05-07 15:22:11 +0700

    update cache system

--- index.js
+++ index.js
@@ -3,36 +3,20 @@ const mesg = require('mesg-js').application({ endpoint })
 const debug = require('debug')('marketplace')
 const error = require('debug')('marketplace:error')
 
-// cache all services and service list on startup.
-getServices().then(({ services }) => {
-  cacheServices(services)
-  services.forEach(async ({ sid }) => cacheService(sid, await getService(sid)))
-})
-
-// update caches by watching marketplace events.
-;[
-  'serviceCreated',
-  'serviceOfferCreated',
-  'serviceOfferDisabled',
-  'serviceOwnershipTransferred',
-  'servicePurchased',
-  'serviceVersionCreated'
-].forEach((eventKey) => {
-  mesg.listenEvent({ serviceID: 'marketplace', eventFilter: eventKey })
-    .on('data', async (event) => { 
-      const { sid } = JSON.parse(event.eventData)
-      cacheServices(await getServices())
-      cacheService(sid, await getService(sid))
-    })
-    .on('error', (err) => {
-      error(`err while listening event ${eventKey}:`, err)
-      process.exit(1)
-    })
-})
+async function refreshAllCache() {
+  const services = await getServices()
+  const servicesToCache = []
+  for (const service in services) {
+    const s = await getService(service.sid)
+    cacheService(s)
+    servicesToCache.push(s)
+  }
+  await cacheServices(services)
+}
 
 // cacheService caches response for GET /services/:sid endpoints.
-function cacheService(sid, service) {
-  return cache('/services/'+sid, service)
+function cacheService(service) {
+  return cache('/services/' + service.sid, service)
 }
 
 // cacheServices caches response for GET /services endpoint.
@@ -56,21 +40,42 @@ function cache(endpoint, content) {
   debug(`cached response for get ${endpoint}`)
 }
 
-function getService(sid) {
-  return revealOutputData(mesg.executeTaskAndWaitResult({
+async function getService(sid) {
+  const { outputData } = await mesg.executeTaskAndWaitResult({
     serviceID: 'marketplace',
     taskKey: 'getService',
     inputData: JSON.stringify({ sid })
-  }))
+  })
+  return JSON.parse(outputData)
 }
 
-function getServices() {
-  return revealOutputData(mesg.executeTaskAndWaitResult({
+async function getServices() {
+  const { outputData } = await mesg.executeTaskAndWaitResult({
     serviceID: 'marketplace',
     taskKey: 'listServices',
     inputData: JSON.stringify({})
-  }))
+  })
+  return JSON.parse(outputData)
 }
+
+refreshAllCache()
+
+  // update caches by watching marketplace events.
+  ;[
+    'serviceCreated',
+    'serviceOfferCreated',
+    'serviceOfferDisabled',
+    'serviceOwnershipTransferred',
+    'servicePurchased',
+    'serviceVersionCreated'
+  ].forEach((eventKey) => {
+    mesg.listenEvent({ serviceID: 'marketplace', eventFilter: eventKey })
+      .on('data', refreshAllCache)
+      .on('error', (err) => {
+        error(`err while listening event ${eventKey}:`, err)
+        process.exit(1)
+      })
+  })
 
 // serve 404 for all(*) other requests.
 mesg.listenEvent({ serviceID: 'http-server', eventFilter: 'request' })
@@ -78,12 +83,12 @@ mesg.listenEvent({ serviceID: 'http-server', eventFilter: 'request' })
     const data = JSON.parse(event.eventData)
     const sessionID = data.sessionID
     try {
-      await revealOutputData(mesg.executeTaskAndWaitResult({
+      await mesg.executeTaskAndWaitResult({
         serviceID: 'http-server',
         taskKey: 'completeSession',
         inputData: JSON.stringify({ sessionID, code: 404 })
-      }))
-    } catch(err) {
+      })
+    } catch (err) {
       error('error while responding api request:', err)
     }
   })
@@ -91,8 +96,3 @@ mesg.listenEvent({ serviceID: 'http-server', eventFilter: 'request' })
     error('error while listening api requests:', err)
     process.exit(1)
   })
-
-// revealOutputData parses and returns output data from successfully finished promise.
-function revealOutputData(promise) {
-  return promise.then(({ outputData }) => JSON.parse(outputData))
-}
